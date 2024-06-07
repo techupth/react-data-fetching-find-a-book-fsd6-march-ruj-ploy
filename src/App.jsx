290c02428d19da2b8d7eafcf7e8e126dc77bd907
AtirujRat <136755600+RattRuj@users.noreply.github.com>
2024-06-07 14:46:42 +0700

    feat:add search book func on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
+import { DebounceInput } from "react-debounce-input";
 
 function App() {
-  return <div className="App">{/* start coding here */}</div>;
+  const [search, setSearch] = useState("");
+  const [bookList, setBookList] = useState([]);
+
+  const fetchBookHandle = async () => {
+    const response = await axios.get(
+      `https://www.googleapis.com/books/v1/volumes?q=${search}`
+    );
+    setBookList(response.data.items);
+  };
+
+  useEffect(() => {
+    fetchBookHandle();
+  }, [search]);
+
+  return (
+    <div className="App">
+      <h1>Find a Book</h1>
+      <DebounceInput
+        minLength={2}
+        debounceTimeout={500}
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <ul>
+        {bookList.map((item) => {
+          return <li>{item.volumeInfo.title}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
